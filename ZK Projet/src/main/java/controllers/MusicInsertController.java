b5874fa07b51f65fa1622216dc15288cff718f6f
Nicolas <nicolasdelponte2012@gmail.com>
2024-08-01 17:32:40 -0300

    Avance nuevo

---
+++ ZK Projet/src/main/java/controllers/MusicInsertController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+public class MusicInsertController extends GenericForwardComposer<Component>  {
+	
+	
+	
+	
+	
+	
+}
